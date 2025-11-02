fc1813a1530a3f53f61ec69b82e6dc51a171cf45
janne6565 <jabbekeipert@gmail.com>
2025-11-02 11:07:17 +0100

    Anonymize metrics

--- src/main/java/com/janne/coveredv2/controller/GameController.java
+++ src/main/java/com/janne/coveredv2/controller/GameController.java
@@ -19,42 +19,40 @@ import java.util.List;
 @RequestMapping(BaseController.BASE_URL + "/games")
 public class GameController {
 
-	private final GameService gameService;
-	private final MetricService metricService;
+    private final GameService gameService;
+    private final MetricService metricService;
 
-	@GetMapping
-	public ResponseEntity<Page<Game>> getAllGames(@PageableDefault(size = 50) Pageable pageable,
-	                                              @RequestParam(value = "search", required = false) String search) {
-		metricService.incrementCounter("app_all_games_requests");
-		return ResponseEntity.ok(gameService.getAllGames(pageable, search));
-	}
+    @GetMapping
+    public ResponseEntity<Page<Game>> getAllGames(@PageableDefault(size = 50) Pageable pageable,
+                                                  @RequestParam(value = "search", required = false) String search) {
+        metricService.incrementCounter("app_all_games_requests");
+        return ResponseEntity.ok(gameService.getAllGames(pageable, search));
+    }
 
-	@GetMapping("/player/{playerId}")
-	public ResponseEntity<GameWithPlaytime[]> getGamesFromPlayer(@PathVariable Long playerId) {
-		GameWithPlaytime[] games = gameService.getGamesFromPlayer(playerId);
-		metricService.incrementCounter("app_get_games_from_player", List.of(
-				"steam_id", String.valueOf(playerId),
-				"game_count", String.valueOf(games.length),
-				"unresolved_games", String.valueOf(
-						Arrays.stream(games).filter(game ->
-								game.getGame().getTimeOfLastCoverFetch() == null
-						).toList().size())
-		));
-		return ResponseEntity.ok(games);
-	}
+    @GetMapping("/player/{playerId}")
+    public ResponseEntity<GameWithPlaytime[]> getGamesFromPlayer(@PathVariable Long playerId) {
+        GameWithPlaytime[] games = gameService.getGamesFromPlayer(playerId);
+        metricService.incrementCounter("app_get_games_from_player", List.of(
+            "game_count", String.valueOf(games.length),
+            "unresolved_games", String.valueOf(
+                Arrays.stream(games).filter(game ->
+                    game.getGame().getTimeOfLastCoverFetch() == null
+                ).toList().size())
+        ));
+        return ResponseEntity.ok(games);
+    }
 
-	@GetMapping("/family/{userId}")
-	public ResponseEntity<GameWithPlaytime[]> getGamesFromSteamFamilyLibrary(@PathVariable Long userId,
-	                                                                         @RequestParam("token") String userApiToken) {
-		GameWithPlaytime[] games = gameService.getGameFromSteamFamilyLibrary(userId, userApiToken);
-		metricService.incrementCounter("app_get_games_from_player_family", List.of(
-				"steam_id", String.valueOf(userId),
-				"game_count", String.valueOf(games.length),
-				"unresolved_games", String.valueOf(
-						Arrays.stream(games).filter(game ->
-								game.getGame().getTimeOfLastCoverFetch() == null
-						).toList().size())
-		));
-		return ResponseEntity.ok(games);
-	}
+    @GetMapping("/family/{userId}")
+    public ResponseEntity<GameWithPlaytime[]> getGamesFromSteamFamilyLibrary(@PathVariable Long userId,
+                                                                             @RequestParam("token") String userApiToken) {
+        GameWithPlaytime[] games = gameService.getGameFromSteamFamilyLibrary(userId, userApiToken);
+        metricService.incrementCounter("app_get_games_from_player_family", List.of(
+            "game_count", String.valueOf(games.length),
+            "unresolved_games", String.valueOf(
+                Arrays.stream(games).filter(game ->
+                    game.getGame().getTimeOfLastCoverFetch() == null
+                ).toList().size())
+        ));
+        return ResponseEntity.ok(games);
+    }
 }
--- src/main/java/com/janne/coveredv2/controller/UtilController.java
+++ src/main/java/com/janne/coveredv2/controller/UtilController.java
@@ -18,34 +18,33 @@ import java.util.List;
 @RequestMapping(BaseController.BASE_URL + "/util")
 public class UtilController {
 
-	private final SteamApiService steamApiService;
-	private final MetricService metricService;
+    private final SteamApiService steamApiService;
+    private final MetricService metricService;
 
-	@GetMapping("/steam/resolve-vanity-url/{vanityUrl}")
-	public ResponseEntity<SteamIdResponse> getUserGameLibrary(@PathVariable String vanityUrl) {
-		String steamId = steamApiService.resolveSteamIdFromSteamVanityUrl(vanityUrl);
-		metricService.incrementCounter("app_resolve_vanity_url", List.of("vanity_url", vanityUrl, "steam_id", String.valueOf(steamId)));
-		return ResponseEntity.ok(SteamIdResponse.builder()
-				.steamid(steamId)
-				.build());
-	}
+    @GetMapping("/steam/resolve-vanity-url/{vanityUrl}")
+    public ResponseEntity<SteamIdResponse> getUserGameLibrary(@PathVariable String vanityUrl) {
+        String steamId = steamApiService.resolveSteamIdFromSteamVanityUrl(vanityUrl);
+        metricService.incrementCounter("app_resolve_vanity_url");
+        return ResponseEntity.ok(SteamIdResponse.builder()
+            .steamid(steamId)
+            .build());
+    }
 
-	@GetMapping("/steam/username/{steamId}")
-	public ResponseEntity<String> getUserName(@PathVariable Long steamId) {
-		String username = steamApiService.getSteamUserName(steamId);
-		metricService.incrementCounter("app_username_requests", List.of("username", username, "steam_id", String.valueOf(steamId)));
-		return ResponseEntity.ok(username);
-	}
+    @GetMapping("/steam/username/{steamId}")
+    public ResponseEntity<String> getUserName(@PathVariable Long steamId) {
+        String username = steamApiService.getSteamUserName(steamId);
+        metricService.incrementCounter("app_username_requests");
+        return ResponseEntity.ok(username);
+    }
 
-	@GetMapping("/steam/profile-validity/{steamId}")
-	public ResponseEntity<ProfileValidityResponse> getProfileValidity(@PathVariable Long steamId) {
-		ProfileValidityResponse profileValidityResponse = steamApiService.isProfileValid(steamId);
-		metricService.incrementCounter("app_profile_validity_requests",
-				List.of(
-						"steam_id", String.valueOf(steamId),
-						"valid", String.valueOf(profileValidityResponse.isValid()),
-						"reason", String.valueOf(profileValidityResponse.getReason())
-				));
-		return ResponseEntity.ok(profileValidityResponse);
-	}
+    @GetMapping("/steam/profile-validity/{steamId}")
+    public ResponseEntity<ProfileValidityResponse> getProfileValidity(@PathVariable Long steamId) {
+        ProfileValidityResponse profileValidityResponse = steamApiService.isProfileValid(steamId);
+        metricService.incrementCounter("app_profile_validity_requests",
+            List.of(
+                "valid", String.valueOf(profileValidityResponse.isValid()),
+                "reason", String.valueOf(profileValidityResponse.getReason())
+            ));
+        return ResponseEntity.ok(profileValidityResponse);
+    }
 }
